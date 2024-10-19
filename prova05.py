ec84093a27b57d40de0b913a6e53c9d97d5af238
Luizln10 <lneto7768@gmail.com>
2024-10-19 14:52:21 -0300

    Update prova05.py

--- prova05.py
+++ prova05.py
@@ -1,14 +1,23 @@
-inicio = int(input("Insira um número do início:"))
-fim = int(input("Insira um número do fim:"))
+alunos = int(input("Digite o número dos alunos:"))
 
-soma = 0
-Pares = False
+somaFinal = 0
 
-for numero in range(inicio, fim + 1):
-    if numero % 2 == 0:
-        soma += numero
-        Pares = True
-if Pares:
-    print(f"A soma dos pares é {soma}")
-else: 
-    print("Não tem números pares")
+for numero in range(alunos):
+    nome = input("Digite o nome do aluno:")
+    nota1 = float(input("Digite a nota 1:"))
+    nota2 = float(input("Digite a nota 2:"))
+    nota3 = float(input("Digite a nota 3:"))
+    media = (nota1 + nota2 + nota3)/3
+
+    somaFinal += media
+
+    if media >= 7:
+        print("Você está aprovado!")
+    else:
+        print("Você está reprovado!")
+    
+    print(f"Nome: {nome}")
+    print(f"Notas: {nota1}, {nota2}, {nota3}")
+    print(f"Média: {media}")
+    mediaFinal = somaFinal / alunos
+    print(f"Média geral: {mediaFinal}")
